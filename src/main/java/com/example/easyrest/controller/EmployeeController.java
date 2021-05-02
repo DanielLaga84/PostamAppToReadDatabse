cbffdf4773e001c0ff82600c52c33a2daa327dad
daniellaga84@gmail.com <Erkano84!!>
2021-05-02 12:32:46 +0200

    Added hateoas to make fullREST Api. Changed GetMapping methods to show links.

--- src/main/java/com/example/easyrest/controller/EmployeeController.java
+++ src/main/java/com/example/easyrest/controller/EmployeeController.java
@@ -3,9 +3,16 @@ package com.example.easyrest.controller;
 import com.example.easyrest.exception.EmployeeNotFoundException;
 import com.example.easyrest.model.Employee;
 import com.example.easyrest.repository.EmployeeRepository;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class EmployeeController {
@@ -15,18 +22,35 @@ public class EmployeeController {
     public EmployeeController(EmployeeRepository repository) {
         this.repository = repository;
     }
+
     @GetMapping("/employees")
-    List<Employee> all() {
-        return repository.findAll();
+    CollectionModel<EntityModel<Employee>> all() {
+
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+                .map(employee -> EntityModel.of(employee,
+                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+                .collect(Collectors.toList());
+        ;
+
+        return CollectionModel.of(employees,
+                linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
+
     }
 
     @PostMapping("/employees")
     Employee newEmployee(@RequestBody Employee newEmployee) {
         return repository.save(newEmployee);
     }
+
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id){
-        return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+    EntityModel<Employee> one(@PathVariable Long id) {
+
+        Employee employee = repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        return EntityModel.of(employee,
+                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
     }
 
     @PutMapping("/employees/{id}")
@@ -37,11 +61,12 @@ public class EmployeeController {
                     employee.setRole(newEmployee.getRole());
                     return repository.save(employee);
                 })
-                .orElseGet(()->{
+                .orElseGet(() -> {
                     newEmployee.setId(id);
                     return repository.save(newEmployee);
                 });
     }
+
     @DeleteMapping("/employees/{id}")
     void deleteEmployee(@PathVariable Long id) {
         repository.deleteById(id);
